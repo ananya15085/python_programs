5250681842449216335d74cafd255d1ca36d701c
ananya15085 <ananya15085@gmail.com>
2026-06-20 16:39:43 +0530

    change string

--- string_2.py
+++ string_2.py
@@ -1,5 +1,28 @@
 greeting = "Hello!"
-print(id(greeting))        #1483337190016
+print(id(greeting))        #2049090472576
 
 greeting = "Hello, World!"
-print(id(greeting))         #2610388401776
+print(id(greeting))         #2049090453104
+
+str = "welcome"
+str = 'welcome'
+str1 = "This is a book on python which discusses all the topics of core python."
+str2 = 'This is a book on python which discusses all the topics of core python.'
+
+str = """ This is a 'core python' book."""
+print(str)
+#This is a 'core python' book.
+
+str = '''This is a "core python" book.'''
+print(str)
+#This is a "core python" book.
+
+s = 'Welcome to Core Python'
+print(s)
+#Welcome to Core Python
+
+print(s[0])          #W
+print(s[3:7])        #come
+print(s[11:])        #Core Python
+print(s[-1])         #n
+print(s*2)
