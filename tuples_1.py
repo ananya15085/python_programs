b7dd62bd0ac1905992eb80e237ad1e71d81f2bea
ananya15085 <ananya15085@gmail.com>
2026-06-20 22:49:56 +0530

    tuples_1.py

---
+++ tuples_1.py
@@ -0,0 +1,11 @@
+tuple = (10, -20, 15.5, 'vijay', 'mary')
+print(tuple)
+#(10, -20, 15.5, 'vijay', 'mary')
+print(tuple[0])        #10
+print(tuple[1:4])      #(-20, 15.5, 'vijay')
+print(tuple[-3])       #15.5
+print(tuple[-1])       #mary
+print(tuple*3)         
+#(10, -20, 15.5, 'vijay', 'mary', 10, -20, 15.5, 'vijay', 'mary', 10, -20, 15.5, 'vijay', 'mary')
+(tuple[0])=99
+print(tuple[0])
